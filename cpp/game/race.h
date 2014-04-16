49a6cc10bee45011339d83ef544611f463ac00ec
Piotr Żurkowski <kareth92@gmail.com>
2014-04-17 00:09:25 +0200

    add game files

---
+++ cpp/game/race.h
@@ -0,0 +1,13 @@
+#ifndef CPP_GAME_RACE_H_
+#define CPP_GAME_RACE_H_
+
+namespace game {
+
+class Race {
+ public:
+ private:
+};
+
+}  // namespace game
+
+#endif  // CPP_GAME_RACE_H_
---
+++ cpp/game/track.h
@@ -0,0 +1,13 @@
+#ifndef CPP_GAME_TRACK_H_
+#define CPP_GAME_TRACK_H_
+
+namespace game {
+
+class Track {
+ public:
+ private:
+};
+
+}  // namespace game
+
+#endif  // CPP_GAME_TRACK_H_
