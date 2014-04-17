6c836e1e513beb21b6212e4a4ad6a2d1dacb6a4a
Piotr Żurkowski <kareth92@gmail.com>
2014-04-17 04:33:21 +0200

    Parse positions

--- cpp/game/position.h
+++ cpp/game/position.h
@@ -1,14 +1,37 @@
 #ifndef CPP_GAME_POSITION_H_
 #define CPP_GAME_POSITION_H_
 
+#include <string>
+#include <iostream>
+#include "jsoncons/json.hpp"
+
 namespace game {
 
 class Position {
  public:
+  Position();
+
+  double angle() const { return angle_; }
+  int piece() const { return piece_index_; }
+  double piece_distance() const { return piece_distance_; }
+
+  int start_lane() const { return start_lane_; }
+  int end_lane() const { return end_lane_; }
+
+  int lap() const { return lap_; }
+
+  // Returns color of the car
+  std::string ParseFromJson(const jsoncons::json& data);
+
  private:
-   double angle_;
-   int piece_index_;
-   double piece_distance_;
+  double angle_;
+  int piece_index_;
+  double piece_distance_;
+
+  int start_lane_;
+  int end_lane_;
+
+  int lap_;
 };
 
 }  // namespace game
