196c25e3562f73eca2cdc4716f23b1e440ff395f
Tomasz Zurkowski <doriath88@gmail.com>
2014-04-18 01:10:27 -0400

    Fix drift model - not perfect yet

--- cpp/game/car_tracker.h
+++ cpp/game/car_tracker.h
@@ -10,6 +10,8 @@
 #include "game/position.h"
 #include "game/gauss.h"
 
+using std::map;
+
 namespace game {
 
 class ErrorTracker {
@@ -34,46 +36,43 @@ class ErrorTracker {
   int error_count_ = 0;
 };
 
+// We assume following drift model
+//
+// angle = x0 * previous_angle + x1 * previous_previous_angle + x2 * previous_velocity
 class DriftModel {
  public:
+  DriftModel() : radius_(0.0) {}
+  DriftModel(double radius) : radius_(radius) {}
+
   ~DriftModel() {
     std::cout << "==== Drift Model ====" << std::endl;
-  }
-
-  void Record(double radius, double angle, double previous_angle) {
-  }
-};
-
-// We assume following velocity model
-//
-// velocity = x * previous_velocity + y * throttle
-class VelocityModel {
- public:
-  ~VelocityModel() {
-    std::cout << "==== Velocity Model ====" << std::endl;
-    std::cout << "x: " << x_ << " y: " << y_ << std::endl;
+    std::cout << "radius: " << radius_ << std::endl;
+    std::cout << "x0: " << x_[0] << " x1: " << x_[1] << " x2: " << x_[2] << std::endl;
     error_tracker_.Print();
+    std::cout << std::endl;
   }
 
-  // velocity = x * previous_velocity + y * previous_throttle
-  void Record(double velocity, double previous_velocity, double previous_throttle) {
-    if (velocity == 0) return;
+  void Record(double angle, double previous_angle,
+              double previous_previous_angle, double previous_velocity) {
+    if (angle == 0) return;
 
-    // Validate the model if ready
     if (IsReady()) {
-      error_tracker_.Add(velocity, Predict(previous_velocity, previous_throttle));
+      error_tracker_.Add(angle, Predict(previous_angle, previous_previous_angle, previous_velocity));
     }
 
-    m_.push_back({velocity, previous_velocity, previous_throttle});
+    if (m_.size() < 3) {
+      //std::cout << "radius: " << radius << "," << angle << "," << previous_angle << "," << previous_previous_angle << "," << previous_velocity << std::endl;
+      m_.push_back({previous_angle, previous_previous_angle, previous_velocity});
+      b_.push_back(angle);
+    }
 
-    if (m_.size() == 2) {
+    if (m_.size() == 3 && !IsReady()) {
       Train();
     }
   }
 
-  // Returns predicted new velocity based on velocity and applied throttle.
-  double Predict(double velocity, double throttle) {
-    return x_ * velocity + y_ * throttle;
+  double Predict(double angle, double previous_angle, double velocity) {
+    return x_[0] * angle + x_[1] * previous_angle + x_[2] * velocity;
   }
 
   bool IsReady() {
@@ -82,23 +81,76 @@ class VelocityModel {
 
  private:
   void Train() {
-    vector<vector<double>> a{{m_[0][1],m_[0][2]},{m_[1][1],m_[1][2]}};
-    vector<double> b{m_[0][0],m_[1][0]} ;
-    vector<double> x;
-
-    GaussDouble(a, b, x);
-
-    x_ = x[0];
-    y_ = x[1];
+    GaussDouble(m_, b_, x_);
     ready_ = true;
   }
 
-  double x_ = 1;
-  double y_ = 0;
+  double radius_ = 0.0;
 
-  // {velocity, previous_velocity, previous_throttle}
   bool ready_ = false;
+  // {previous_angle, previous_previous_angle, previous_velocity, angle}
   vector<vector<double> > m_;
+  vector<double> b_;
+
+  vector<double> x_;
+
+  ErrorTracker error_tracker_;
+};
+
+// We assume following velocity model
+//
+// velocity = x0 * previous_velocity + x1 * throttle
+class VelocityModel {
+ public:
+  ~VelocityModel() {
+    std::cout << "==== Velocity Model ====" << std::endl;
+    std::cout << "x0: " << x_[0] << " x1: " << x_[1] << std::endl;
+    error_tracker_.Print();
+    std::cout << std::endl;
+  }
+
+  // velocity = x * previous_velocity + y * previous_throttle
+  void Record(double velocity, double previous_velocity, double previous_throttle) {
+    if (velocity == 0) return;
+
+    if (IsReady()) {
+      error_tracker_.Add(velocity, Predict(previous_velocity, previous_throttle));
+    }
+
+    if (m_.size() < 2) {
+      m_.push_back({previous_velocity, previous_throttle});
+      b_.push_back(velocity);
+    }
+
+    if (m_.size() == 2 && !IsReady()) {
+      Train();
+    }
+  }
+
+  // Returns predicted new velocity based on velocity and applied throttle.
+  double Predict(double velocity, double throttle) {
+    return x_[0] * velocity + x_[1] * throttle;
+  }
+
+  bool IsReady() {
+    return ready_;
+  }
+
+ private:
+  void Train() {
+    GaussDouble(m_, b_, x_);
+    ready_ = true;
+  }
+
+  bool ready_ = false;
+
+  // Variables used to train the model
+  // {previous_velocity, previous_throttle} {velocity}
+  vector<vector<double> > m_;
+  vector<double> b_;
+
+  // Model used for prediction.
+  vector<double> x_;
 
   ErrorTracker error_tracker_;
 };
@@ -110,6 +162,7 @@ class CrashModel {
     std::cout << "==== Crash Model ====" << std::endl;
     std::cout << "threshold: " << angle_threshold_ << std::endl;
     std::cout << (ready_ ? "with crash" : "without crash") << std::endl;
+    std::cout << std::endl;
   }
 
   void RecordCarCrash(double angle) {
@@ -138,6 +191,10 @@ class CrashModel {
   bool ready_ = false;
   double angle_threshold_ = 0.0;
 };
+
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
 class CarTracker {
  public:
@@ -174,13 +231,24 @@ class CarTracker {
     // Update Models
     crash_model_.Record(angle);
     velocity_model_.Record(velocity, velocity_, throttle_);
+    GetDriftModel(position)->Record(angle, angle_, previous_angle_, velocity_);
 
     // Update state
+    previous_angle_ = angle_;
     angle_ = angle;
     velocity_ = velocity;
     positions_.push_back(position);
 
     LogState();
+  }
+
+  DriftModel* GetDriftModel(const Position& position) {
+    auto& piece = race_->track().pieces().at(position.piece());
+    double radius = piece.radius() * sgn(piece.angle());
+    if (drift_model_[radius] == nullptr) {
+      drift_model_[radius].reset(new DriftModel(radius));
+    }
+    return drift_model_[radius].get();
   }
 
   void RecordThrottle(double throttle) {
@@ -214,11 +282,15 @@ class CarTracker {
 
   double velocity_ = 0;
   double angle_ = 0;
+  double previous_angle_ = 0;
 
   const Race* race_;
 
   CrashModel crash_model_;
   VelocityModel velocity_model_;
+  // We need separate drift models for different pieces. We group them by
+  // radius (straight pieces assume radius 0).
+  map<double, std::unique_ptr<DriftModel>> drift_model_;
 
   vector<Position> positions_;
   double throttle_ = 0;
--- cpp/game/car_tracker_test.cpp
+++ cpp/game/car_tracker_test.cpp
@@ -33,4 +33,25 @@ TEST_F(VelocityModelTest, FinlandTrack) {
   EXPECT_DOUBLE_EQ(0.29404, velocity_model_.Predict(0.198, 0.5));
 }
 
+TEST(DriftTest, Basic) {
+  // Data came from t=0.6 on Finland track
+  vector<double> angle{0, 0, -0.20219, -0.585046, -1.12784, -1.81073, -2.61479, -3.52209, -4.51571, -5.57977, -6.69946, -7.86103, -9.05176, -10.26, -11.4752, -12.6876, -13.8887};
+  vector<double> velocity{5.97162, 5.97162, 5.97274, 5.97329, 5.97382, 5.97434, 5.97486, 5.97536, 5.97585, 5.97634, 5.97681, 5.97727, 5.97773, 5.97773, 5.97861, 5.97904, 5.97946};
+
+  DriftModel drift_model_;
+
+  // Record 3 first values to train the model.
+  for (int i = 0; i < 3; ++i) {
+    EXPECT_FALSE(drift_model_.IsReady());
+    drift_model_.Record(angle[i + 2], angle[i + 1], angle[i], velocity[i + 1]);
+  }
+
+  EXPECT_TRUE(drift_model_.IsReady());
+
+  // Error should be less than 0.001
+  for (int i = 5; i < angle.size(); ++i) {
+    EXPECT_NEAR(angle[i], drift_model_.Predict(angle[i-1], angle[i-2], velocity[i-1]), 0.001);
+  }
+}
+
 }  // namespace game
