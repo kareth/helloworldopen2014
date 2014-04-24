99d931c4083a4ec2fa24e8a013d84cc964098b7b
Piotr Żurkowski <kareth92@gmail.com>
2014-04-24 03:13:36 +0200

    Pick optimal straigth for turbo

--- cpp/schedulers/greedy_turbo_scheduler.cpp
+++ cpp/schedulers/greedy_turbo_scheduler.cpp
@@ -27,24 +27,67 @@ void GreedyTurboScheduler::Schedule(const game::CarState& state) {
     if (state.position().lap() == race_.laps() - 1)
       should_fire_now_ = true;
   }
+
+  // TODO its kind of greedy,
+  // first of all - we shouldnt count just straights
+  // second, it has to make some kind of decisions based on turbo freq
+
+  if (strategy_ == Strategy::kOptimizeRace) {
+    printf("Longest\n");
+    // Longest overall
+    if (state.position().piece() == straights_[0].from()) {
+      should_fire_now_ = true;
+    }
+  } else if (strategy_ == Strategy::kOptimizeCurrentLap) {
+    printf("Current\n");
+    // Longest in between now and lap end
+    for (auto& s : straights_) {
+      if (s.from() == state.position().piece())
+        should_fire_now_ = true;
+      if (s.from() > state.position().piece())
+        return;
+    }
+  } else if (strategy_ == Strategy::kOptimizeNextLap) {
+    printf("Next\n");
+    // Give him best speed for next lap
+    // TODO not optimal, just taking last piece
+    auto last = &straights_[0];
+    for (auto& s : straights_)
+      if (s.from() > last->from())
+        last = &s;
+
+    // If connected to 0
+    if (last->to() == race_.track().pieces().size() - 1)
+      if (last->from() == state.position().piece())
+        should_fire_now_ = true;
+  }
 }
 
 void GreedyTurboScheduler::FindLongestStraights() {
-  /*const auto& pieces = race_.track().pieces();
-
+  const auto& pieces = race_.track().pieces();
 
   int from = -1;
   double length = 0;
   for (int i = 0; i < pieces.size(); i++) {
-    if (pieces[i].type() == PieceType::kStraight) {
+    if (pieces[i].type() == game::PieceType::kStraight) {
       if (from == -1)
         from = i;
       length += pieces[i].length();
     } else {
-      if (from != -1);
+      if (from != -1) {
+        straights_.push_back(Straight(length, from, i - 1));
+        length = 0;
+        from = -1;
+      }
     }
   }
-*/
+  if (from != -1)
+    straights_.push_back(Straight(length, from, pieces.size() - 1));
+
+  sort(straights_.begin(), straights_.end(), [](const Straight& a, const Straight& b) {
+      if (a.length() != b.length())
+        return a.length() > b.length();
+      return a.from() < b.from(); } );
 }
 
 // Prepare for overtake
