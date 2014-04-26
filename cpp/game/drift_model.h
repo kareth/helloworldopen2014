fd2ca7e24479fd2c7c87d4651466b5af540cc19f
Tomasz Zurkowski <doriath88@gmail.com>
2014-04-26 13:11:09 -0400

    Create drift_model.cpp

--- cpp/game/drift_model.h
+++ cpp/game/drift_model.h
@@ -23,87 +23,17 @@ namespace game {
 // angle = x0 * previous_angle + x1 * previous_previous_angle + x2 * previous_velocity + x3
 class DriftModel {
  public:
-  DriftModel() {
-    x_ = {1.9, -0.9, -0.00125, 0.00125 * sqrt(180000), 0.3};
+  DriftModel();
 
-    char filename[50];
-    sprintf (filename, "bin/drift.csv");
-    file_.open (filename);
-    file_ << "angle,p_angle,velocity,radius,direction,next_angle,error" << std::endl;
-  }
-
-  ~DriftModel() {
-    std::cout << "==== Drift Model ====" << std::endl;
-    if (IsReady()) {
-      for (int i = 0; i < x_.size(); i++)
-        std::cout << "x" << i <<": " << x_[i] << " ";
-      std::cout << std::endl;
-
-      std::cout << "Model trained using: " << std::endl;
-      for (int i = 0; i < model_.size(); ++i) {
-        for (int j = 0; j < model_[i].size(); ++j) std::cout << std::setprecision(20) << model_[i][j] << " ";
-        std::cout << std::endl;
-      }
-    }
-    error_tracker_.Print();
-    std::cout << std::endl;
-
-    file_.close();
-  }
+  ~DriftModel();
 
   // next_angle = f(angle, previous_previous_angle, previous_velocity, radius, direction)
-  void Record(double next_angle, double angle, double previous_angle, double velocity, double radius, double direction) {
-    if (next_angle == 0) return;
-
-
-    // TODO keep improving the model
-    // TODO remove hardcoding of the next_angle. Maybe we should use crash model?
-    if (!IsReady() && direction != 0 && next_angle > 5) {
-      model_.push_back({
-          angle,
-          previous_angle,
-          velocity * angle,
-          -direction * velocity * velocity * sqrt(InvRadius(radius)),
-          direction * velocity
-      });
-      b_.push_back(next_angle);
-      // TODO
-      if (model_.size() > 8) {
-        Train();
-      }
-    }
-    double predicted = 0;
-    if (IsReady()) {
-      predicted = Predict(angle, previous_angle, velocity, radius, direction);
-      error_tracker_.Add(predicted, next_angle);
-    }
-
-    file_ << std::setprecision(20) << angle << "," << previous_angle << "," << velocity << "," << radius << "," << direction << "," << next_angle << "," << predicted - next_angle << std::endl;
-  }
+  void Record(double next_angle, double angle, double previous_angle, double velocity, double radius, double direction);
 
   // direction = {-1, 0, 1}
-  double Predict(double angle, double previous_angle, double velocity, double radius, double direction) {
-    // if (!IsReady()) return angle;
-    return x_[0] * angle +
-           x_[1] * previous_angle +
-           x_[2] * velocity * angle +
-           -direction * fmax(0, x_[3] * velocity * velocity * sqrt(InvRadius(radius)) - x_[4] * velocity);
-  }
+  double Predict(double angle, double previous_angle, double velocity, double radius, double direction);
 
-  double EstimateRadius(double next_angle, double angle, double previous_angle, double velocity, double direction) {
-    double r =
-      1 / pow((next_angle
-            - x_[0] * angle +
-            - x_[1] * previous_angle +
-            - x_[2] * velocity * angle +
-            - direction * x_[4] * velocity) / (-direction * x_[3] * velocity * velocity), 2);
-
-    if (r > 0 && x_[3] * velocity * velocity * sqrt(1/r) - x_[4] * velocity < 1e-5) {
-      return 0;
-    }
-
-    return r;
-  }
+  double EstimateRadius(double next_angle, double angle, double previous_angle, double velocity, double direction);
 
   bool IsReady() const {
     return ready_;
