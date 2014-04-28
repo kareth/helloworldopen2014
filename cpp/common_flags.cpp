dab1602258fe042caad0e301b5b2a61fd3616db1
Tomasz Zurkowski <doriath88@gmail.com>
2014-04-28 17:53:23 -0400

    Turn on --check_if_safe_ahead by default

--- cpp/common_flags.cpp
+++ cpp/common_flags.cpp
@@ -4,7 +4,7 @@ DEFINE_string(race_id, "", "");
 DEFINE_bool(continuous_integration, true, "");
 DEFINE_double(safe_angle, 60.0, "");
 DEFINE_int32(handicap, 0, "");
-DEFINE_bool(check_if_safe_ahead, false, "");
+DEFINE_bool(check_if_safe_ahead, true, "");
 DEFINE_int32(answer_time, 10, "Time limit for answer in ms");
 
 
