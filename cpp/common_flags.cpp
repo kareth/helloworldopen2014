0040a491784ce7057ec098183f9745333a3a993f
Tomasz Zurkowski <doriath88@gmail.com>
2014-04-28 22:01:48 -0400

    Enable bump with turbo by default

--- cpp/common_flags.cpp
+++ cpp/common_flags.cpp
@@ -8,5 +8,5 @@ DEFINE_bool(check_if_safe_ahead, true, "");
 DEFINE_int32(answer_time, 10, "Time limit for answer in ms");
 
 
-DEFINE_bool(bump_with_turbo, false, "kamikazebumps");
+DEFINE_bool(bump_with_turbo, true, "kamikazebumps");
 DEFINE_bool(defend_turbo_bump, false, "Defend against people that have bump_with_turbo");
