d553870db85166838a2e1b93343881eef1aab270
Tomasz Zurkowski <doriath88@gmail.com>
2014-05-25 14:17:56 +0200

    Fix drift model

--- cpp/game/drift_model.cpp
+++ cpp/game/drift_model.cpp
@@ -69,9 +69,54 @@ void DriftModel::Record(double next_angle, double angle, double previous_angle, 
 }
 
 void DriftModel::Train() {
-  TrainOnlyOneVariable();
+  // We will solve A * x = b
+  vector<vector<double> > A;
+  vector<double> b;
+  vector<double> x;
 
-  // TODO train the full model
+  for (int i = (int) raw_points_.size() - 1; i >= 0; --i) {
+    double angle = raw_points_[i][0];
+    double previous_angle = raw_points_[i][1];
+    double velocity = raw_points_[i][2];
+    double radius = raw_points_[i][3];
+    double direction = raw_points_[i][4];
+    double next_angle = raw_points_[i][5];
+
+    // Ignore points if velocity is too small or we are on straigh piece (the
+    // first x0, x1, x2 are enough to compute next_angle). We need points were
+    // we can assume that we can drop the "max" factor in the model.
+    if (ready_ && fabs(next_angle - (x_[0] * angle + x_[1] * previous_angle + x_[2] * angle * velocity)) < 1e-9) {
+      continue;
+    }
+    if (direction == 0) {
+      continue;
+    }
+
+    A.push_back({
+        velocity * angle,
+        -direction * velocity * velocity * sqrt(InvRadius(radius)),
+        direction * velocity});
+    b.push_back(next_angle - (x_[0] * angle + x_[1] * previous_angle));
+
+    if (b.size() >= 10) break;
+  }
+
+  // We use 4 points instead of 3 just to make sure we do not learn model
+  // on incorrect ones.
+  if (b.size() >= 4) {
+    double error = Approximation(A, b, x);
+
+    // If chosen points are incorrect, then do not learn model. Wait for better
+    // points.
+    if (error < 1e-9) {
+      ready_ = true;
+      x_ = {x_[0], x_[1], x[0], x[1], x[2]};
+    }
+
+    if (b.size() >= 10) {
+      very_ready_ = true;
+    }
+  }
 }
 
 double DriftModel::ComputeMaxError() {
