51dba26e65faf1d4b945776dc8abb2517c1f5897
Wojciech Jaśkowski <wojciech.jaskowski@gmail.com>
2014-05-30 14:31:16 +0200

    Magic scheduler uses python script

--- cpp/schedulers/magic_throttle_scheduler.cpp
+++ cpp/schedulers/magic_throttle_scheduler.cpp
@@ -1,12 +1,18 @@
 #include "schedulers/magic_throttle_scheduler.h"
 
 #include <chrono>
+#include <cmath>
+#include <sstream>
+#include <unistd.h>
+#include <cstdio>
+#include <streambuf>
 
 #include "gflags/gflags.h"
 
 namespace schedulers {
 
 using game::CarState;
+using game::CarTracker;
 
 const int MagicThrottleScheduler::HORIZON = 50;
 const int MagicThrottleScheduler::N = 100;
@@ -30,8 +36,60 @@ void MagicThrottleScheduler::Schedule(const game::CarState& state) {
   Log(state);
 }
 
+void WriteDoubleVector(std::ostringstream& os, const vector<double>& v) {
+  os << "\"";
+  for (int i=0; i<v.size(); ++i) {
+    if (i != 0) os << " ";
+    os << v[i];
+  }
+  os << "\"";
+}
+
+void WriteCurves(std::ostringstream& os, const vector<CarTracker::Curve>& v) {
+  os << "\"";
+  for (int i=0; i<v.size(); ++i) {
+    if (i != 0) os << " ";
+    os << v[i].direction << " " << v[i].rradius() << " " << v[i].length;
+  }
+  os << "\"";
+}
+
+vector<double> MakeMagic(int n, int horizon, const vector<double>& vm, const vector<double>& dm, double max_drift, double v0, double a_1, double a0, const vector<CarTracker::Curve>& curves, const vector<double>& initial_x) {
+
+  std::ostringstream cmd;
+   
+  cmd << "python magic.py ";
+  cmd << n << " " << horizon << " ";
+  WriteDoubleVector(cmd, vm); cmd << " ";
+  WriteDoubleVector(cmd, dm); cmd << " ";
+  cmd << max_drift << " " << v0 << " " << a_1 << " " << a0 << " ";
+  WriteCurves(cmd, curves); cmd << " ";
+  WriteDoubleVector(cmd, initial_x);
+
+  //TODO: Is this OK? Can I have any unexpected problems with this? NULL?
+  FILE* f = popen(cmd.str().c_str(), "r");
+  vector<double> x;
+  double v;
+  while (fscanf(f, "%lf", &v) != EOF) {
+    x.push_back(v);
+  }
+  pclose(f);
+  //TODO: Check whether x has size of n. What else?
+
+  return x;
+}
+
 void MagicThrottleScheduler::ImproveByMagic(const game::CarState& state, Sched& schedule) {
-    //TODO
+  vector<double> dm = car_tracker_.GetDriftModel().GetModel();
+  vector<double> vm = car_tracker_.GetVelocityModel().GetModel();
+  double max_drift = car_tracker_.GetCrashModel().GetModel();
+
+  //TODO: Appropriate distance
+  vector<CarTracker::Curve> curves = car_tracker_.GetCurves(state, 1000); 
+
+  schedule.throttles = MakeMagic(N, HORIZON, vm, dm, max_drift, state.velocity(),
+          state.previous_angle(), state.position().angle(), curves, schedule.throttles);
+  schedule.UpdateDistance(state);
 }
 
 void MagicThrottleScheduler::Log(const game::CarState& state) {
